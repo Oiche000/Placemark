c9a114638ce9b2c0da4d3a6f348678ceacc9b266
Oiche000 <20114793@mail.wit.ie>
2026-03-14 12:08:56 +0000

    add boolean isAdmin to user in mongo and joy schemas

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -12,11 +12,13 @@ export const UserCredentialsSpec = Joi.object()
 export const UserSpec = UserCredentialsSpec.keys({
   firstName: Joi.string().example("Homer").required(),
   lastName: Joi.string().example("Simpson").required(),
+
 }).label("UserDetails");
 
 export const UserSpecPlus = UserSpec.keys({
   _id: IdSpec,
   __v: Joi.number(),
+  isAdmin: Joi.boolean().default(false),
 }).label("UserDetailsPlus");
 
 export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
